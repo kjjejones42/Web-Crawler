41f04a4ee0172f31a5bd6fe0207b264e59bfc1ce
kjjejones42 <kjjejones42@gmail.com>
2020-02-23 14:34:43 +0000

    test 1

--- src/crawler/WebCrawler.java
+++ src/crawler/WebCrawler.java
@@ -9,8 +9,17 @@ public class WebCrawler extends JFrame {
     public WebCrawler() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
+
+        JTextArea textarea = new JTextArea();
+        textarea.setName("TextArea");
+        textarea.setText("HTML code?");
+        textarea.setEnabled(false);
+        add(textarea);
+
+        setTitle("Window");
+
         setVisible(true);
-    }    
+    }
 
     public static void main(final String[] args) {
         new WebCrawler();
